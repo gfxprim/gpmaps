75bf0d93bdbef6e05cd6cae1ef94c5f5e50f12a9
Cyril Hrubis <metan@ucw.cz>
2021-07-15 21:58:09 +0200

    Fix map layer struct indentation.
    Signed-off-by: Cyril Hrubis <metan@ucw.cz>

--- xqx_map_layer.h
+++ xqx_map_layer.h
@@ -15,19 +15,18 @@
 
 #include "xqx_view.h"
 
-struct xqx_map_layer
-{
-  struct xqx_view_layer common;
+struct xqx_map_layer {
+	struct xqx_view_layer common;
 
-  struct xqx_map *map;
-  struct xqx_map_cache_client *cc;
+	struct xqx_map *map;
+	struct xqx_map_cache_client *cc;
 
-  int pix_off_x, pix_off_y;
-  uint32_t level;
+	int pix_off_x, pix_off_y;
+	uint32_t level;
 
-  uint32_t tx1, tx2, tx3, tx4, ty1, ty2, ty3, ty4;
-  uint32_t t2x1, t2x2, t2y1, t2y2;
-  uint32_t ax, ay, as;
+	uint32_t tx1, tx2, tx3, tx4, ty1, ty2, ty3, ty4;
+	uint32_t t2x1, t2x2, t2y1, t2y2;
+	uint32_t ax, ay, as;
 };
 
 struct xqx_map_layer *xqx_make_map_layer(struct xqx_map *map);
